e1bd217804e89dc7a50e280ab3e9d6b8b262ccc3
jackswift21 <33332432+jackswift21@users.noreply.github.com>
2018-02-09 10:17:31 -0600

    Demo

--- public/scripts/toggle-sidebar.js
+++ public/scripts/toggle-sidebar.js
@@ -5,10 +5,12 @@ function open_sidebar(){
 		.removeClass("w3-animate-left-hide")
 		.addClass("w3-animate-left")
 		.show();}
-	else close_sidebar()();}
+	else close_sidebar();}
 function close_sidebar(){
 	mySidebar
 	.removeClass("w3-animate-left")
 	.addClass("w3-animate-left-hide");
 	setTimeout(() => mySidebar.hide(),400);
 }
+
+//sidebar close hiccups on route change, ugh.
