285bafa86064d0098ce6327afa6635a7f485644a
jackswift21 <33332432+jackswift21@users.noreply.github.com>
2018-02-15 18:22:03 -0600

    first one

--- my-mailer.js
+++ my-mailer.js
@@ -10,14 +10,6 @@ var transporter = nodemailer.createTransport({
     clientSecret:'jMbnARYyagEGQFAU20YJX7uc',
     refreshToken:'1/4lmB8R6PGRkXu0Lzh897N0jmcAod9UaZwY3jopAax6k',
     accessToken: 'ya29.GltjBV-HwYJagubkpJhe5qu3cUIg3xw2W7YqbIuxmvFYBUTutVwgPgorjS6nZI8XtFdu6LHossSg-HpKsQLl1pDKl9HdD1mtJhPePsY4slE9nwGzwpEda6W7DKS3'}});
-/*var mailOptions = {
-  from: ,
-  to: 'jlpowersjr@aol.com',
-  subject: 'Test Email',
-  text: 'This is a test email from JNAJ Api Service. '+
-    'Click here to confirm acknowledge your receipt of this email. '+
-    'DO NOT RESPOND TO EMAIL DIRECTLY. '+
-    'Thank you, Jack.'};*/
 module.exports = {
   send:function({_from,_to,_subject,_html,_text}){
     return new Promise(done => {
@@ -26,7 +18,7 @@ module.exports = {
         to: _to,
         subject: _subject,
         html: _html,
-        text: _text };
+        text: _text};
       transporter.sendMail(mailOptions,(err,info) => {
         if(err) return here(err);
-        done('Message ' + info.messageId + ' sent: %s' + info.response);});});}}
+        done('Message '+info.messageId+' sent: %s'+info.response);});});}}
